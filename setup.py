b011cd399efd634524c3f07120c60404ae1567ea
adammck <adam.mckaig@gmail.com>
2010-04-01 22:13:50 -0400

    added setup. distutils is still garbage.

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# vim: et ts=4 sw=4
+
+
+from distutils.core import setup
+
+
+setup(
+    name="django-app-settings",
+    version="0.1.0",
+    license="CC0",
+
+    author="Adam Mckaig",
+    author_email="adam.mckaig@gmail.com",
+
+    description="Allow reusable Django apps to provide settings",
+    url="http://github.com/adammck/django-app-settings",
+    packages=["djangoappsettings"])
